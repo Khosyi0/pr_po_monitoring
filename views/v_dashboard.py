b97f4b4f0e582aaf332ab201cdaa3754120ea955
Achmad Khosyi' A. R. <ramandanta@gmail.com>
2026-04-10 14:47:51 +0700

    Update v_dashboard.py

--- views/v_dashboard.py
+++ views/v_dashboard.py
@@ -218,19 +218,18 @@ def render(filter_conditions, bagian_pr_cond, bagian_po_cond, load_data, **kwarg
                 "key": "kpi_estimasi",
                 "icon_path": "M4 10.781c.148 1.667 1.513 2.85 3.591 3.003V15h1.043v-1.216c2.27-.179 3.678-1.438 3.678-3.3 0-1.59-.947-2.51-2.956-3.028l-.722-.187V3.467c1.122.11 1.879.714 2.07 1.616h1.47c-.166-1.6-1.54-2.748-3.54-2.875V1H7.591v1.233c-1.939.23-3.27 1.472-3.27 3.156 0 1.454.966 2.483 2.661 2.917l.61.162v4.031c-1.149-.17-1.94-.8-2.131-1.718zm3.391-3.836c-1.043-.263-1.6-.825-1.6-1.616 0-.944.704-1.641 1.8-1.828v3.495l-.2-.05zm1.591 1.872c1.287.323 1.852.859 1.852 1.769 0 1.097-.826 1.828-2.2 1.939V8.73z",
                 "label": "Total Estimasi PR",
-                "value": format_idr(estimasi_pr_all),
+                "value": format_idr(oe_po_val),
                 "delta": "Owner's Estimate (OE)",
                 "formula": f"""\
 **Total Estimasi PR (OE)**: Total nilai OE dari semua PR.
 
-**Total Estimasi PR saat ini:** {int(estimasi_pr_all):,}
+**Total Estimasi PR saat ini:** {int(oe_po_val):,}
 
-**Formula Excel:** (PR SAP)
-- Filter **1St Full Release** selain `blanks`
+**Formula Excel:** (PO SAP)
 - Filter **Material No** selain `1000076`
-- Filter **PO Deletion Flag** selain `X`
-- Filter **Account Assignment** selain `U`
-- Hitung jumlah **Estimasi PR**
+- Filter **PO Deletion Flag** selain `L`
+- Buat kolom **OE**: `= Quantity PR × Estimasi PR`
+- Hitung jumlah **OE**
 
 **Target:** -\
 """,
